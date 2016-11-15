af3b539cdbe69d1697acd7733055e8c624510742
BaddMann <baddmann@myjmo.com>
2016-11-15 13:01:16 -0700

    My Debug Line was removed
    I was using that line to figure out why I was getting Whitespace. Forgot to remove it I guess

--- 2.6_Audit_Count.sh
+++ 2.6_Audit_Count.sh
@@ -3,5 +3,4 @@
 # Security Reporting - Count Risks
 
 auditfile=/Library/Application\ Support/SecurityScoring/org_audit
-reportcount=$(cat "$auditfile" | grep "*" | wc -l | tr -d '[:space:]')
 echo "<result>$(cat "$auditfile" | grep "*" | wc -l | tr -d '[:space:]')</result>"
